ac3368ba26711731313f4c69294ca394d7bcb67a
vlvrft <rufickk@gmail.com>
2023-04-02 23:03:38 +0400

    Add Final task

---
+++ FinalTask/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Clear();
+
+string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+void LessThanThreeSymbol(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    System.Console.WriteLine();
+}
+
+LessThanThreeSymbol(array1, array2);
+PrintArray(array2);
+
+
